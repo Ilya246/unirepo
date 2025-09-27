94d0e7139c0437d269278811c73ffee87431b065
polinazabolotskikh <pzobolotik@gmail.com>
2025-09-27 16:31:06 +0400

    сделала TabulatedFunction

---
+++ src/main/java/ru/ssau/tk/_AMEBA_/_PESEZ_/functions/TabulatedFunction.java
@@ -0,0 +1,30 @@
+package ru.ssau.tk._AMEBA_._PESEZ_.functions;
+
+public interface TabulatedFunction {
+    double apply(double x);
+
+    //Метод получения количества табулированных значений
+    int getCount();
+
+    //Метод, получающий значение аргумента x по номеру индекса
+    double getX(int index);
+
+    //Метод, получающий значение y по номеру индекса
+    double getY(int index);
+
+    //Метод, задающий значение y по номеру индекса
+    void setY(int index, double value);
+
+    //Метод, возвращающий индекс аргумента x
+    int indexOfX(double x);
+
+    //Метод, возвращающий индекс первого вхождения значения y
+    int indexOfY(double y);
+
+    //Метод, возвращающий самый левый x
+    double leftBound();
+
+    //Метод, возвращающий самый правый x
+    double rightBound();
+
+}
