7221adea7c1312d01bcb33e16d09d1b5afe9d651
polinazabolotskikh <pzobolotik@gmail.com>
2025-09-27 18:48:38 +0400

    сделала LinkedListTabulatedFunction

---
+++ src/main/java/ru/ssau/tk/_AMEBA_/_PESEZ_/functions/LinkedListTabulatedFunction.java
@@ -0,0 +1,155 @@
+package ru.ssau.tk._AMEBA_._PESEZ_.functions;
+
+public class LinkedListTabulatedFunction extends AbstractTabulatedFunction{
+    private  int count;
+    private  Node head;
+    private void addNode(double x, double y){
+        Node newNode = new Node(x, y);
+        if(head==null){
+            head=newNode;
+            head.next=head;
+            head.prev=head;
+        }
+        else{
+            Node last = head.prev;
+            last.next=newNode;
+            newNode.prev=last;
+            newNode.next=head;
+            head.prev=newNode;
+        }
+       count++;
+    }
+
+    public LinkedListTabulatedFunction(double[] xValues, double[] yValues) {
+        if (xValues.length != yValues.length) {
+            throw new IllegalArgumentException("Размеры массивов не совпадают");
+        }
+        if (xValues.length < 2) {
+            throw new IllegalArgumentException("Должно быть хотя бы 2 точки");
+        }
+        for (int i = 1; i < xValues.length; i++) {
+            if (xValues[i] <= xValues[i - 1]) {
+                throw new IllegalArgumentException("xValues должны быть упорядочены");
+            }
+        }
+        for (int i = 0; i < xValues.length; i++) {
+            addNode(xValues[i], yValues[i]);
+        }
+    }
+
+    public LinkedListTabulatedFunction(MathFunction source, double xFrom, double xTo, int count){
+        if (count < 2) {
+            throw new IllegalArgumentException("Должно быть хотя бы 2 точки");
+        }
+        if(xFrom>xTo){
+            double temp = xFrom;
+            xFrom=xTo;
+            xTo=temp;
+        }
+        double step=(xTo-xFrom)/(count-1);
+        for(int i=0; i<count; i++){
+            double x=xFrom+i*step;
+            addNode(x, source.apply(x));
+        }
+    }
+
+    @Override
+    public int floorIndexOfX(double x) {
+        if (x < head.x) return 0;
+        if (x > head.prev.x) return count;
+
+        Node current = head;
+        int index = 0;
+        while (index < count - 1 && current.next.x <= x) {
+            current = current.next;
+            index++;
+        }
+        return index;
+    }
+
+    @Override
+    public double extrapolateLeft(double x) {
+        return interpolate(x, head.x, head.next.x, head.y, head.next.y);
+    }
+
+    @Override
+    public double extrapolateRight(double x) {
+        return interpolate(x, head.prev.prev.x, head.prev.x, head.prev.prev.y, head.prev.y);
+    }
+
+    @Override
+    public double interpolate(double x, int floorIndex) {
+        Node left = getNode(floorIndex);
+        Node right = left.next;
+        return interpolate(x, left.x, right.x, left.y, right.y);
+    }
+
+    @Override
+    public int getCount() {
+        return count;
+    }
+
+    private Node getNode(int index){
+        if (index<0 || index>=count){
+            throw new IndexOutOfBoundsException();
+        }
+        Node current;
+        if (index<count/2){
+            current=head;
+            for(int i=0;i<index;i++){
+                current=current.next;
+            }
+        }else{
+            current=head.prev;
+            for(int i=count-1;i>index;i--){
+                current=current.prev;
+            }
+        }
+        return current;
+    }
+
+    @Override
+    public double getX(int index) {
+        return getNode(index).x;
+    }
+
+    @Override
+    public double getY(int index) {
+        return getNode(index).y;
+    }
+
+    @Override
+    public void setY(int index, double value) {
+        getNode(index).y=value;
+    }
+
+    @Override
+    public int indexOfX(double x) {
+        Node current=head;
+        for (int i=0; i<count; i++){
+            if (current.x==x) return i;
+            current=current.next;
+        }
+        return -1;
+    }
+
+    @Override
+    public int indexOfY(double y) {
+        Node current=head;
+        for (int i=0; i<count; i++){
+            if (current.y==y) return i;
+            current=current.next;
+        }
+        return -1;
+    }
+
+    @Override
+    public double leftBound() {
+        return head.x;
+    }
+
+    @Override
+    public double rightBound() {
+        return head.prev.x;
+    }
+}
---
+++ src/test/java/ru/ssau/tk/_AMEBA_/_PESEZ_/test/LinkedListTabulatedFunctionTest.java
@@ -0,0 +1,142 @@
+package ru.ssau.tk._AMEBA_._PESEZ_.test;
+
+import org.junit.jupiter.api.Test;
+import ru.ssau.tk._AMEBA_._PESEZ_.functions.LinkedListTabulatedFunction;
+import ru.ssau.tk._AMEBA_._PESEZ_.functions.MathFunction;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LinkedListTabulatedFunctionTest {
+
+
+
+    private static class LinearFunction implements MathFunction {
+        @Override
+        public double apply(double x) {
+            return 2 * x + 1;
+        }
+    }
+
+    private double[] xValues = {0, 1, 2};
+    private double[] yValues = {1, 3, 5};
+
+    @Test
+    public void testArrays(){
+        LinkedListTabulatedFunction func=new LinkedListTabulatedFunction(xValues,yValues);
+        assertEquals(3, func.getCount());
+        assertEquals(0, func.getX(0));
+        assertEquals(1, func.getX(1));
+        assertEquals(2, func.getX(2));
+
+        assertEquals(1, func.getY(0));
+        assertEquals(3, func.getY(1));
+        assertEquals(5, func.getY(2));
+    }
+
+    @Test
+    public void testFunction() {
+        LinearFunction f = new LinearFunction();
+        LinkedListTabulatedFunction func = new LinkedListTabulatedFunction(f, 0, 2, 3);
+
+        assertEquals(3, func.getCount());
+        assertEquals(0, func.getX(0));
+        assertEquals(1, func.getX(1));
+        assertEquals(2, func.getX(2));
+
+        assertEquals(1, func.getY(0));
+        assertEquals(3, func.getY(1));
+        assertEquals(5, func.getY(2));
+    }
+
+    @Test
+    void floorIndexOfX() {
+        LinkedListTabulatedFunction func = new LinkedListTabulatedFunction(xValues, yValues);
+        assertEquals(0, func.floorIndexOfX(0)); // точное совпадение
+        assertEquals(0, func.floorIndexOfX(0.5)); // между 0 и 1
+        assertEquals(1, func.floorIndexOfX(1.5)); // между 1 и 2
+        assertEquals(3, func.floorIndexOfX(3)); // больше правой границы
+        assertEquals(0, func.floorIndexOfX(-1)); // меньше левой границы
+    }
+
+    @Test
+    void extrapolateLeft() {
+        LinkedListTabulatedFunction func = new LinkedListTabulatedFunction(xValues, yValues);
+        assertEquals(-1, func.extrapolateLeft(-1)); // по формуле линейного продолжения
+        assertEquals(1, func.extrapolateLeft(0));   // совпадение с первой точкой
+    }
+
+    @Test
+    void extrapolateRight() {
+        LinkedListTabulatedFunction func = new LinkedListTabulatedFunction(xValues, yValues);
+        assertEquals(7, func.extrapolateRight(3));  // линейное продолжение вправо
+        assertEquals(5, func.extrapolateRight(2));  // совпадение с последней точкой
+    }
+
+    @Test
+    void interpolate() {
+        LinkedListTabulatedFunction func = new LinkedListTabulatedFunction(xValues, yValues);
+        assertEquals(2, func.interpolate(0.5, 0)); // между 0 и 1
+        assertEquals(4, func.interpolate(1.5, 1)); // между 1 и 2
+        assertEquals(1, func.interpolate(0, 0));   // точное совпадение с узлом
+        assertEquals(5, func.interpolate(2, 1));   // точное совпадение с последним узлом
+    }
+
+    @Test
+    void getCount() {
+        LinkedListTabulatedFunction func = new LinkedListTabulatedFunction(xValues, yValues);
+        assertEquals(3, func.getCount());
+    }
+
+    @Test
+    void getX() {
+        LinkedListTabulatedFunction func = new LinkedListTabulatedFunction(xValues, yValues);
+        assertEquals(0, func.getX(0));
+        assertEquals(1, func.getX(1));
+        assertEquals(2, func.getX(2));
+    }
+
+    @Test
+    void getY() {
+        LinkedListTabulatedFunction func = new LinkedListTabulatedFunction(xValues, yValues);
+        assertEquals(1, func.getY(0));
+        assertEquals(3, func.getY(1));
+        assertEquals(5, func.getY(2));
+    }
+
+    @Test
+    void setY() {
+        LinkedListTabulatedFunction func = new LinkedListTabulatedFunction(xValues, yValues);
+        func.setY(1, 10);
+        assertEquals(10, func.getY(1));
+    }
+
+    @Test
+    void indexOfX() {
+        LinkedListTabulatedFunction func = new LinkedListTabulatedFunction(xValues, yValues);
+        assertEquals(0, func.indexOfX(0));
+        assertEquals(1, func.indexOfX(1));
+        assertEquals(2, func.indexOfX(2));
+        assertEquals(-1, func.indexOfX(5));
+    }
+
+    @Test
+    void indexOfY() {
+        LinkedListTabulatedFunction func = new LinkedListTabulatedFunction(xValues, yValues);
+        assertEquals(0, func.indexOfY(1));
+        assertEquals(1, func.indexOfY(3));
+        assertEquals(2, func.indexOfY(5));
+        assertEquals(-1, func.indexOfY(10));
+    }
+
+    @Test
+    void leftBound() {
+        LinkedListTabulatedFunction func = new LinkedListTabulatedFunction(xValues, yValues);
+        assertEquals(0, func.leftBound());
+    }
+
+    @Test
+    void rightBound() {
+        LinkedListTabulatedFunction func = new LinkedListTabulatedFunction(xValues, yValues);
+        assertEquals(2, func.rightBound());
+    }
+}
