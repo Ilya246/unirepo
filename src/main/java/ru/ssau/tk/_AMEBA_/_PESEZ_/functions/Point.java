1b4f63e464a472009f22e9abcb4014abe86129c9
Ilya246 <ilyukarno@gmail.com>
2025-10-02 20:50:13 +0400

    Y: Point

---
+++ src/main/java/ru/ssau/tk/_AMEBA_/_PESEZ_/functions/Point.java
@@ -0,0 +1,10 @@
+package ru.ssau.tk._AMEBA_._PESEZ_.functions;
+
+public class Point {
+    double x, y;
+
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+}
