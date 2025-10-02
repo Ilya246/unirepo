f6d0d31b5880061cc6d29dcfea0966a0a020f107
polinazabolotskikh <pzobolotik@gmail.com>
2025-10-02 21:14:16 +0400

    перенесла Node

--- src/main/java/ru/ssau/tk/_AMEBA_/_PESEZ_/functions/LinkedListTabulatedFunction.java
+++ src/main/java/ru/ssau/tk/_AMEBA_/_PESEZ_/functions/LinkedListTabulatedFunction.java
@@ -1,6 +1,19 @@
 package ru.ssau.tk._AMEBA_._PESEZ_.functions;
 
 public class LinkedListTabulatedFunction extends AbstractTabulatedFunction implements Removable, Insertable{
+
+    static class Node {
+        public Node next;
+        public Node prev;
+        public double x;
+        public double y;
+
+        public Node(double x, double y){
+            this.x=x;
+            this.y=y;
+        }
+    }
+
     private  int count;
     private  Node head;
     private void addNode(double x, double y){
