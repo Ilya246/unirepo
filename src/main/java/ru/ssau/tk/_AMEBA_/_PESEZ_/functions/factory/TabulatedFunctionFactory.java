c8f4fd06844a7682b71f98bc4bca5725f28cb6fc
Ilya246 <ilyukarno@gmail.com>
2025-10-04 19:09:56 +0400

    Y: improve tabulated test, add createUnmodifiable()

--- src/main/java/ru/ssau/tk/_AMEBA_/_PESEZ_/functions/factory/TabulatedFunctionFactory.java
+++ src/main/java/ru/ssau/tk/_AMEBA_/_PESEZ_/functions/factory/TabulatedFunctionFactory.java
@@ -1,7 +1,13 @@
 package ru.ssau.tk._AMEBA_._PESEZ_.functions.factory;
 
 import ru.ssau.tk._AMEBA_._PESEZ_.functions.TabulatedFunction;
+import ru.ssau.tk._AMEBA_._PESEZ_.functions.UnmodifiableTabulatedFunction;
 
 public interface TabulatedFunctionFactory {
     TabulatedFunction create(double[] xValues, double[] yValues);
+
+    default TabulatedFunction createUnmodifiable(double[] xValues, double[] yValues) {
+        TabulatedFunction func = create(xValues, yValues);
+        return new UnmodifiableTabulatedFunction(func);
+    }
 }
---
+++ src/test/java/ru/ssau/tk/_AMEBA_/_PESEZ_/test/TabulatedFunctionFactoryTest.java
@@ -0,0 +1,72 @@
+package ru.ssau.tk._AMEBA_._PESEZ_.test;
+
+import org.junit.jupiter.api.Test;
+import ru.ssau.tk._AMEBA_._PESEZ_.functions.*;
+import ru.ssau.tk._AMEBA_._PESEZ_.functions.factory.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TabulatedFunctionFactoryTest {
+
+    @Test
+    void testCreateArray() {
+        var factory = new ArrayTabulatedFunctionFactory();
+
+        TabulatedFunction function = testFactory(factory);
+
+        assertInstanceOf(ArrayTabulatedFunction.class, function);
+    }
+
+    @Test
+    void testCreateLinkedList() {
+        var factory = new LinkedListTabulatedFunctionFactory();
+
+        TabulatedFunction function = testFactory(factory);
+
+        assertInstanceOf(LinkedListTabulatedFunction.class, function);
+    }
+
+    @Test
+    void testCreateUnmodifiableArray() {
+        var factory = new ArrayTabulatedFunctionFactory();
+
+        testUnmodifiable(factory);
+    }
+
+    @Test
+    void testCreateUnmodifiableList() {
+        var factory = new LinkedListTabulatedFunctionFactory();
+
+        testUnmodifiable(factory);
+    }
+
+    TabulatedFunction testFactory(TabulatedFunctionFactory factory) {
+        double[] xValues = {1.0, 2.0, 3.0};
+        double[] yValues = {10.0, 20.0, 30.0};
+
+        TabulatedFunction function = factory.create(xValues, yValues);
+        testFunction(function);
+
+        return function;
+    }
+
+    void testUnmodifiable(TabulatedFunctionFactory factory) {
+        double[] xValues = {1.0, 2.0, 3.0};
+        double[] yValues = {10.0, 20.0, 30.0};
+
+        TabulatedFunction function = factory.createUnmodifiable(xValues, yValues);
+
+        testFunction(function);
+        assertThrows(UnsupportedOperationException.class, () -> function.setY(0, 5));
+    }
+
+    void testFunction(TabulatedFunction function) {
+        assertEquals(3, function.getCount());
+        assertEquals(1.0, function.getX(0));
+        assertEquals(2.0, function.getX(1));
+        assertEquals(3.0, function.getX(2));
+        assertEquals(10.0, function.getY(0));
+        assertEquals(20.0, function.getY(1));
+        assertEquals(30.0, function.getY(2));
+    }
+}
