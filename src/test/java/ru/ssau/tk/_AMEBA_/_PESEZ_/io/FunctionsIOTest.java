c86f792d0d8cae9e8ce3f6b3b09fab053cd9b88b
polinazabolotskikh <pzobolotik@gmail.com>
2025-10-05 18:39:42 +0400

    тест

--- src/test/java/ru/ssau/tk/_AMEBA_/_PESEZ_/io/FunctionsIOTest.java
+++
@@ -1,9 +0,0 @@
-package ru.ssau.tk._AMEBA_._PESEZ_.io;
-
-import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-class FunctionsIOTest {
-
-}
---
+++ src/test/java/ru/ssau/tk/_AMEBA_/_PESEZ_/test/FunctionsIOTest.java
@@ -0,0 +1,5 @@
+package ru.ssau.tk._AMEBA_._PESEZ_.test;
+
+class FunctionsIOTest {
+
+}
