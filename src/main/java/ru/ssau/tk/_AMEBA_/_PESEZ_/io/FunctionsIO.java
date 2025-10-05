f526225de69ac8a18d927ff6afc32a34ed92d824
Ilya246 <ilyukarno@gmail.com>
2025-10-05 17:20:25 +0400

    Y: TabulatedFunctionFileReader

--- src/main/java/ru/ssau/tk/_AMEBA_/_PESEZ_/io/FunctionsIO.java
+++ src/main/java/ru/ssau/tk/_AMEBA_/_PESEZ_/io/FunctionsIO.java
@@ -1,7 +1,11 @@
 package ru.ssau.tk._AMEBA_._PESEZ_.io;
 
 import java.io.*;
+import java.text.*;
+import java.util.Locale;
+
 import ru.ssau.tk._AMEBA_._PESEZ_.functions.*;
+import ru.ssau.tk._AMEBA_._PESEZ_.functions.factory.TabulatedFunctionFactory;
 
 public final class FunctionsIO {
     private FunctionsIO() {
@@ -16,4 +20,25 @@ public final class FunctionsIO {
         }
         print.flush();
     }
+
+    public static TabulatedFunction readTabulatedFunction(BufferedReader reader, TabulatedFunctionFactory factory) throws IOException {
+        int count = Integer.parseInt(reader.readLine());
+        var xValues = new double[count];
+        var yValues = new double[count];
+
+        try {
+            var formatter = NumberFormat.getInstance(Locale.forLanguageTag("ru"));
+            for (int i = 0; i < count; i++) {
+                String line = reader.readLine();
+                String[] numbers = line.split(" ");
+                xValues[i] = formatter.parse(numbers[0]).doubleValue();
+                yValues[i] = formatter.parse(numbers[1]).doubleValue();
+            }
+        } catch (ParseException error) {
+            throw new IOException(error);
+        }
+
+        return factory.create(xValues, yValues);
+    }
+
 }
