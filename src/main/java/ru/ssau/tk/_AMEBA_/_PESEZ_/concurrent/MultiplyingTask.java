a1f9a1fe9fd74e9a9015fff5c406fe77123098f9
Ilya246 <ilyukarno@gmail.com>
2025-10-10 16:38:54 +0400

    Y: syncronized() for MultiplyingTask, test race condition

--- src/main/java/ru/ssau/tk/_AMEBA_/_PESEZ_/concurrent/MultiplyingTask.java
+++ src/main/java/ru/ssau/tk/_AMEBA_/_PESEZ_/concurrent/MultiplyingTask.java
@@ -3,7 +3,7 @@ package ru.ssau.tk._AMEBA_._PESEZ_.concurrent;
 import ru.ssau.tk._AMEBA_._PESEZ_.functions.TabulatedFunction;
 
 public class MultiplyingTask implements Runnable {
-    private TabulatedFunction function;
+    private final TabulatedFunction function;
 
     public MultiplyingTask(TabulatedFunction function) {
         this.function = function;
@@ -13,7 +13,9 @@ public class MultiplyingTask implements Runnable {
     public void run() {
         int count = function.getCount();
         for (int i = 0; i < count; i++) {
-            function.setY(i, function.getY(i) * 2);
+            synchronized (function) {
+                function.setY(i, function.getY(i) * 2);
+            }
         }
         System.out.println("Thread " + Thread.currentThread().getName() + " finished MultiplyingTask");
     }
