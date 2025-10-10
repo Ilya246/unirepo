9723fce370b5f8fbafc5e4bb53a8604a0195fbe5
polinazabolotskikh <pzobolotik@gmail.com>
2025-10-10 16:40:25 +0400

    concurrent

--- src/main/java/ru/ssau/tk/_AMEBA_/_PESEZ_/concurrent/ReadTask.java
+++ src/main/java/ru/ssau/tk/_AMEBA_/_PESEZ_/concurrent/ReadTask.java
@@ -1,4 +1,18 @@
 package ru.ssau.tk._AMEBA_._PESEZ_.concurrent;
 
-public class ReadTask {
+import ru.ssau.tk._AMEBA_._PESEZ_.functions.TabulatedFunction;
+
+public class ReadTask implements Runnable{
+    TabulatedFunction function;
+
+    public ReadTask(TabulatedFunction tabulatedFunction) {
+        this.function = tabulatedFunction;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i< function.getCount(); i++){
+            System.out.printf("After read: i = %d, x = %f, y = %f\n", i, function.getX(i), function.getY(i));
+        }
+    }
 }
