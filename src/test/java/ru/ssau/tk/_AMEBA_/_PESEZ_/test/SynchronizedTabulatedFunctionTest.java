9bb61b9822e5b42ec1f775220bc3165721f48e11
Ilya246 <ilyukarno@gmail.com>
2025-10-10 17:54:22 +0400

    X or Y: SynchronizedTabulatedFunction

---
+++ src/test/java/ru/ssau/tk/_AMEBA_/_PESEZ_/test/SynchronizedTabulatedFunctionTest.java
@@ -0,0 +1,57 @@
+package ru.ssau.tk._AMEBA_._PESEZ_.test;
+
+import org.junit.jupiter.api.Test;
+import ru.ssau.tk._AMEBA_._PESEZ_.functions.*;
+import ru.ssau.tk._AMEBA_._PESEZ_.concurrent.SynchronizedTabulatedFunction;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SynchronizedTabulatedFunctionTest {
+    double[] xValues = new double[]{1, 2, 3};
+    double[] yValues = new double[]{10, 20, 30};
+
+    @Test
+    void testWrapArrayTable() {
+        ArrayTabulatedFunction _func = new ArrayTabulatedFunction(xValues.clone(), yValues.clone());
+        SynchronizedTabulatedFunction syncFunc = new SynchronizedTabulatedFunction(_func);
+
+        realAssert(syncFunc);
+    }
+
+    @Test
+    void testWrapLinkedListTable() {
+        LinkedListTabulatedFunction _func = new LinkedListTabulatedFunction(xValues.clone(), yValues.clone());
+        SynchronizedTabulatedFunction syncFunc = new SynchronizedTabulatedFunction(_func);
+
+        realAssert(syncFunc);
+    }
+
+    void integerAssert(TabulatedFunction func) {
+        int i = 1;
+        for (Point p : func) {
+            assertEquals(p.getX(), i);
+            assertEquals(p.getY(), i * 10);
+            i++;
+        }
+        assertEquals(10, func.apply(1));
+        assertEquals(20, func.apply(2));
+        assertEquals(30, func.apply(3));
+        assertEquals(1, func.leftBound());
+        assertEquals(3, func.rightBound());
+        assertEquals(2, func.getX(1));
+        assertEquals(20, func.getY(1));
+        assertEquals(1, func.indexOfX(2));
+        assertEquals(1, func.indexOfY(20));
+        assertEquals(3, func.getCount());
+        func.setY(0, 5);
+        assertEquals(5, func.getY(0));
+        func.setY(0, 10);
+    }
+
+    void realAssert(TabulatedFunction func) {
+        integerAssert(func);
+        assertEquals(-5, func.apply(-0.5));
+        assertEquals(25, func.apply(2.5));
+        assertEquals(45, func.apply(4.5));
+    }
+}
