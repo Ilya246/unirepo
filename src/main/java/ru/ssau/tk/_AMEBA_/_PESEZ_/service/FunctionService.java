40bccd8621c8ec10ab83e5bc17af4dcc3e775300
Ilya246 <ilyukarno@gmail.com>
2025-10-25 18:04:34 +0400

    create FunctionService.java

---
+++ src/main/java/ru/ssau/tk/_AMEBA_/_PESEZ_/service/FunctionService.java
@@ -0,0 +1,10 @@
+package ru.ssau.tk._AMEBA_._PESEZ_.service;
+
+import ru.ssau.tk._AMEBA_._PESEZ_.functions.*;
+
+public class FunctionService {
+    // Запись функции в базу данных
+    public static void WriteFunction(MathFunction function) {
+        
+    }
+}
