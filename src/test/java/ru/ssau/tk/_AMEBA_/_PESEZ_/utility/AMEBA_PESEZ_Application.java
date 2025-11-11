3a56e0ced3d3dc2c2e0dcb397527e9dd96cf4fe8
polinazabolotskikh <pzobolotik@gmail.com>
2025-11-11 22:20:34 +0400

    подключила Spring Boot

---
+++ src/test/java/ru/ssau/tk/_AMEBA_/_PESEZ_/utility/AMEBA_PESEZ_Application.java
@@ -0,0 +1,9 @@
+package ru.ssau.tk._AMEBA_._PESEZ_.utility;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class AMEBA_PESEZ_Application {
+    public static void main(String[] args) {
+        SpringApplication.run(AMEBA_PESEZ_Application.class, args);
+    }
+}
