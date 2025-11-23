83ca75b9e68ef61782c6fb7fe4d39faee8e61c31
polinazabolotskikh <pzobolotik@gmail.com>
2025-11-23 17:06:59 +0400

    получение id

--- src/main/java/ru/ssau/tk/_AMEBA_/_PESEZ_/controllers/crud/UserController.java
+++ src/main/java/ru/ssau/tk/_AMEBA_/_PESEZ_/controllers/crud/UserController.java
@@ -3,13 +3,18 @@ package ru.ssau.tk._AMEBA_._PESEZ_.controllers.crud;
 import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 import ru.ssau.tk._AMEBA_._PESEZ_.dto.request.UserRequest;
 import ru.ssau.tk._AMEBA_._PESEZ_.dto.response.UserResponse;
+import ru.ssau.tk._AMEBA_._PESEZ_.entity.UserEntity;
 import ru.ssau.tk._AMEBA_._PESEZ_.enums.UserType;
 import ru.ssau.tk._AMEBA_._PESEZ_.service.interfaces.UserService;
 
 import java.util.List;
+
+import static ru.ssau.tk._AMEBA_._PESEZ_.utility.Utility.Log;
 
 @RestController
 @RequestMapping("/users")
@@ -19,9 +24,7 @@ public class UserController {
 
     @PostMapping
     @Operation(summary = "Создание пользователя")
-    public Long createUser(@RequestBody @Valid UserRequest request /*@RequestParam String adminUserName,
-                                          @RequestParam String adminPassword*/) {
-        /*userService.authenticateWithRole(adminUserName, adminPassword, UserType.Admin);*/
+    public Long createUser(@RequestBody @Valid UserRequest request ) {
         return userService.createUser(request);
     }
 
@@ -39,26 +42,65 @@ public class UserController {
 
     @PutMapping
     @Operation(summary = "Обновление пользователя")
-    public void updateUser(@RequestParam Long id, @RequestBody @Valid UserRequest request /*@RequestParam String adminUserName,
-                                          @RequestParam String adminPassword*/) {
-        /*userService.authenticateWithRole(adminUserName, adminPassword, UserType.Admin);*/
+    public void updateUser(@RequestParam Long id, @RequestBody @Valid UserRequest request ) {
+
         userService.updateUser(id, request);
     }
 
     @DeleteMapping
     @Operation(summary = "Удаление пользователя")
-    public void deleteUser(@RequestParam Long id /*@RequestParam String adminUserName,
-                                          @RequestParam String adminPassword*/) {
-        /* userService.authenticateWithRole(adminUserName, adminPassword, UserType.Admin);*/
+    public void deleteUser(@RequestParam Long id ) {
         userService.deleteUser(id);
     }
 
     @GetMapping
     @Operation(summary = "Получение всех пользователей")
-    public List<UserResponse> getAllUsers(/*@RequestParam String adminUserName,
-                                          @RequestParam String adminPassword*/ ){
-        /*userService.authenticateWithRole(adminUserName, adminPassword, UserType.Admin);*/
+    public List<UserResponse> getAllUsers(){
         return userService.getAllUsers();
+    }
+
+    @GetMapping("/self")
+    @Operation(summary = "Получение текущего id")
+    public Long getId(){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication == null || !authentication.isAuthenticated()) {
+            throw new RuntimeException("User not authenticated");
+        }
+
+        Object principal = authentication.getPrincipal();
+        Object credentials = authentication.getCredentials();
+
+        if (principal instanceof UserEntity) {
+            return ((UserEntity) principal).getUserId();
+        } else if (principal instanceof org.springframework.security.core.userdetails.User) {
+            org.springframework.security.core.userdetails.User securityUser =
+                    (org.springframework.security.core.userdetails.User) principal;
+            String username = securityUser.getUsername();
+
+            // Если есть credentials (пароль), используем findByCredentials
+            if (credentials instanceof String) {
+                String password = (String) credentials;
+                try {
+                    UserEntity user = userService.findByCredentials(username, password);
+                    return user.getUserId();
+                } catch (Exception e) {
+                    // Fallback to findByUsername если findByCredentials не сработал
+                    Log.warn("findByCredentials failed, falling back to findByUsername: {}", e.getMessage());
+                    UserEntity user = userService.findByUsername(username);
+                    return user.getUserId();
+                }
+            } else {
+                // Используем findByUsername если нет пароля
+                UserEntity user = userService.findByUsername(username);
+                return user.getUserId();
+            }
+        } else if (principal instanceof String) {
+            String username = (String) principal;
+            UserEntity user = userService.findByUsername(username);
+            return user.getUserId();
+        } else {
+            throw new RuntimeException("Unknown principal type: " + principal.getClass().getName());
+        }
     }
 
 }
