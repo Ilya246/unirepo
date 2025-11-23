2e9c635a84a7ea3380dfd1d41ee7344e4f24088b
Ilya246 <ilyukarno@gmail.com>
2025-11-23 23:40:07 +0400

    fix json expects

--- src/main/resources/templates/js/api.js
+++ src/main/resources/templates/js/api.js
@@ -222,7 +222,7 @@ class APIClient {
     async assignOwnership(functionId, name, targetUserId) {
         return this.request(`/owned-functions/own?id=${functionId}&name=${encodeURIComponent(name)}`, {
             method: 'POST'
-        });
+        }, false);
     }
 
     async updateOwnedFunction(id, name, targetUserId = null) {
@@ -247,7 +247,7 @@ class APIClient {
     }
 
     async createPoint(functionId, x, y) {
-        return this.request(`/points?id=${functionId}&x=${x}&y=${y}`, { method: 'POST' });
+        return this.request(`/points?id=${functionId}&x=${x}&y=${y}`, { method: 'POST' }, false);
     }
 
     async updatePoint(functionId, x, y) {
